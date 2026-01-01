f4540dc39073e69c2e2d5de15f3e1969048787bd
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-01-01 22:17:00 +0545

    taught js

---
+++ jspart1/script.js
@@ -0,0 +1,119 @@
+// kunai pani var le le tme aauta variable banauxau vanye teslai tmele
+//  bannu aagi nai access garna sakxau rw tesko value chai undefined aauxa
+
+
+// const, let ma ni hoisting chai hunxa but ini hru le k vanx vanye hamilai declare garnu 
+// vandaa mathi ko part saab dead zone ho so if we try to access that variable 
+// before declaring we get the error "cannot access the value before initialization"
+
+//Reference Value -> array, object, function
+// let a = [1,11,12];
+// b = a;
+// b.pop()
+// console.log("a is:",a)
+// console.log("b is:",b)
+
+
+// let user = {
+//     name: "Laxman"
+// };
+// let user2 = user;
+// user2.name = "Bishal";
+// console.log(user)
+
+
+//Primitive Datatype -> string, number, boolean, null, bigInt
+// let laxman = "Cake";
+// let bishal = laxman;
+// bishal = "HALF-CAKE";
+// console.log(bishal)
+// console.log(laxman)
+
+
+// string = "Bishal" -> string  num = 12; isLogin: true/false; -> boolean  float=12.233;  
+
+// symbol -> bholi  
+// BigInt ->
+// a = 17835875387318765387263382;
+// console.log(a)
+
+// Dynamic Typing -> 
+// let a = 12;
+// let int = "Laxman";
+// console.log(int)
+
+// console.log("ram" == "hari");
+// console.log(1 === 1);
+
+//concatenation
+// console.log(1+2);
+
+// console.log(22*2);
+
+
+// truthy value - all others are truthy value
+// falsy value - 0, false, "", null, undefined, NaN, document.all
+
+// if(0){
+//   console.log("true")
+// }else{
+//  console.log("false hobiusgidu")
+// }
+
+// operators 
+
+// Arthematic operator 
+// console.log(12+3)
+// console.log(6-5)
+// console.log(5*4)
+// console.log(10%2)
+
+//Comparison
+// == -> checks values but not their types
+// === -> strictly checks values and their types
+// > -> greater than  
+// >= -> greater than is equal to 10 >= 10 
+// < -> less than
+// <= -> less than or equal
+// != -> not strict not equal
+// !== -> strict checks not equal
+
+
+// Assignment
+ // a =12;
+ // a += 10;
+ // a -= 10;
+
+//Logical
+
+// AND -> (&&)  == both conditions must be true
+// OR  -> (||)  == both side true = true, any one condition true = true, if both condition are false than false
+// NOT -> (!)   == true xa vanye false parxa fasle xa vanye true
+
+
+// if(!(2==3)){
+//  console.log(true)
+// }else{
+// console.log(false)
+// }
+
+//Unary 
+// pre-post increment
+// pre-post decrement
+
+
+//Tenary operator
+
+// ? -> true
+// : => false
+
+// user = false;
+// console.log(user ? user : "404 error")
+
+// console.log(user?"hii":"404 back res");
+
+
+
+
+
+
